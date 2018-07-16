9069ec818b52e1b212b157cb39853c2017daff96
Sebastián Gurin <sebastigurin@gmail.com>
2018-07-16 02:24:32 -0300

    parse-javap test

--- examples/reflection-test.js
+++ examples/reflection-test.js
@@ -5,3 +5,11 @@ java.classpath.push("./lucene-lib/lucene-queryparser-7.4.0.jar");
 
 
 var idx = java.newInstanceSync("org.apache.lucene.store.RAMDirectory");
+
+debugger;
+const methods = idx.class.getMethodsSync()
+methods.forEach(m=>console.log(m.getNameSync()))
+
+// for(Method method : methods){
+//     System.out.println("method = " + method.getName());
+// }
