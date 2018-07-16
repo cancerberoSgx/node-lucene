ff1f1d066d9c43f70633ced01682c981b4510593
Sebastián Gurin <sebastigurin@gmail.com>
2018-07-16 12:03:08 -0300

    javapjsonoutput

--- javap-json-output/java-src/JavapJsonOutput.java
+++ javap-json-output/java-src/JavapJsonOutput.java
@@ -45,20 +45,20 @@ public class JavapJsonOutput {
     List<String> list = new ArrayList<String>();
     // ArrayList<String> result = {};
     if (Modifier.isPublic(m)) {
-      System.out.print("public ");
+      // System.out.print("public ");
       list.add("public");
       // result.
     }
     if (Modifier.isStatic(m)) {
-      System.out.print("static ");
+      // System.out.print("static ");
       list.add("static");
     }
     if (Modifier.isAbstract(m)) {
-      System.out.print("abstract ");
+      // System.out.print("abstract ");
       list.add("abstract");
     }
     if (Modifier.isFinal(m)) {
-      System.out.print("final ");
+      // System.out.print("final ");
       list.add("final");
     }
     // if(Modifier.isNative(m)){
@@ -72,100 +72,147 @@ public class JavapJsonOutput {
     }
     // TODO: there are other modifiers but not necesary for my project java2js
     // return "";
-    // System.out.print("final ");
+    //// System.out.print("final ");
     return list;
   }
 
-  public String getSuperClass(String s) {
-    try {
-      // Class subclass = Class.forName(s);
-      Class superclass = this.c.getSuperclass();
-      System.out.print("extends " + superclass.getName() + " ");
-      return superclass.getName();
-    } catch (Exception e) {
+  public OutSuperClass getSuperClass() {
+    // try {
+    // Class subclass = Class.forName(s);
+    Class superclass = this.c.getSuperclass();
+    if(superclass==null){
+      return null;
     }
-    return "";
+    OutSuperClass s = new OutSuperClass();
+      s.name = superclass.getName();
+      s.typeParameters = this.getTypeParameters(superclass.getTypeParameters());
+    // System.out.print("extends " + superclass.getName() + " ");
+    // return superclass.getName();
+    // } catch (Exception e) {
+    // }
+    // return "";
+    return s;
   }
 
-  public List<String> getInterfaces(String s) {
-    List<String> list = new ArrayList<String>();
-    try {
-      // Class c = Class.forName(s);
-      Class inter[] = this.c.getInterfaces();
-      System.out.print("implements ");
-      for (int i = 0; i < inter.length; i++) {
-        list.add(inter[i].getName());
-        System.out.print(inter[i].getName());
-        System.out.print(", ");
-      }
-      System.out.println(" ");
-      System.out.println(" { ");
-    } catch (Exception e) {
+  public List<OutImplementedInterface> getInterfaces() {
+    List<OutImplementedInterface> list = new ArrayList<OutImplementedInterface>();
+    // try {
+    // Class c = Class.forName(s);
+    Class inter[] = this.c.getInterfaces();
+    // System.out.print("implements ");
+    for (int i = 0; i < inter.length; i++)   {
+      OutImplementedInterface interf = new OutImplementedInterface();
+      list.add(interf);
+      interf.name = inter[i].getName();
+      interf.typeParameters = this.getTypeParameters(inter[i].getTypeParameters());
+      
+      // System.out.print(inter[i].getName());
+      // System.out.print(", ");
     }
+    // System.out.println(" ");
+    // System.out.println(" { ");
+    // } catch (Exception e) {
+    // }
     return list;
   }
 
-  public List<OutField> getFields(String s) {
+  public List<OutField> getFields() {
     List<OutField> list = new ArrayList<OutField>();
-    try {
-      // Class c = Class.forName(s);
-      Field f[] = this.c.getFields();
-      for (int i = 0; i < f.length; i++) {
-        OutField field = new OutField();
-        list.add(field);
-        // int m = f[i].getModifiers();
-        field.modifiers = getModifiers(f[i].getModifiers());
-        System.out.print(" ");
-        Class type = f[i].getType();
-        System.out.print(type.getName());
-        field.type = type.getName();
-        field.name = f[i].getName();
-        System.out.println(" " + f[i].getName());
-      }
-    } catch (Exception e) {
+    // try {
+    // Class c = Class.forName(s);
+    Field f[] = this.c.getFields();
+    for (int i = 0; i < f.length; i++) {
+      OutField field = new OutField();
+      list.add(field);
+      // int m = f[i].getModifiers();
+      field.modifiers = getModifiers(f[i].getModifiers());
+      // System.out.print(" ");
+      // Class type = f[i].getType();
+      // System.out.print(type.getName());
+      field.type = getType(f[i].getType());//.getName();
+      field.name = f[i].getName();
+      // field.typeParameters = this.getTypeParameters(f[i].getTypeParameters());
+      // System.out.println(" " + f[i].getName());
     }
+    // } catch (Exception e) {
+    // }
     return list;
   }
 
-  public void getConstructors(String s) {
-    try {
-      // Class c = Class.forName(s);
-      Constructor cs[] = this.c.getConstructors();
-      for (int i = 0; i < cs.length; i++) {
-        int m = cs[i].getModifiers();
-        getModifiers(m);
-        System.out.print(this.c.getName() + "(");
-        Class type[] = cs[i].getParameterTypes();
-        for (int k = 0; k < type.length; k++) {
-          System.out.print(type[k].getName() + ",");
-        }
-        System.out.print(")");
-        System.out.println(" ");
-      }
-    } catch (Exception e) {
+  public List<OutMethod> getConstructors() {
+    // try {
+    // Class c = Class.forName(s);
+    List<OutMethod> list = new ArrayList<OutMethod>();
+    Constructor cs[] = this.c.getDeclaredConstructors();
+    for (int i = 0; i < cs.length; i++) {
+      OutMethod om = new OutMethod();
+      list.add(om);
+      om.modifiers = getModifiers(cs[i].getModifiers());
+      om.parameters = getParameters(cs[i].getParameters());
+      om.typeParameters = this.getTypeParameters(cs[i].getTypeParameters());
     }
+    // } catch (Exception e) {
+    // }
+    return list;
   }
 
-  public void getMethods(String s) {
-    try {
-      // Class c = Class.forName(s);
-      Method m[] = this.c.getMethods();
-      for (int i = 0; i < m.length; i++) {
-        int m1 = m[i].getModifiers();
-        getModifiers(m1);
-        System.out.print(m[i].getReturnType().getName());
-        System.out.print(" " + m[i].getName());
-        System.out.print("(");
-        Class type[] = m[i].getParameterTypes();
-        for (int k = 0; k < type.length; k++) {
-          System.out.print(type[k].getName() + ",");
-        }
-        System.out.print(")");
-        System.out.println(" ");
-      }
-    } catch (Exception e) {
-    }
+  public List<OutParam> getParameters(Parameter[] params) {
+    List<OutParam> outParams = new ArrayList<OutParam>();
+    for (int k = 0; k < params.length; k++) {
+      OutParam op = new OutParam();
+      outParams.add(op);
+      op.type = getType(params[k].getType());//.getName();
+      op.name = params[k].getName();
+      op.modifiers = getModifiers(params[k].getModifiers());
 
+      // System.out.print(type[k].getName() + ",");
+    }
+    return outParams;
+  }
+
+  public List<OutMethod> getMethods() {
+    List<OutMethod> list = new ArrayList<OutMethod>();
+    // try {
+    // Class c = Class.forName(s);
+    Method m[] = this.c.getDeclaredMethods();
+    for (int i = 0; i < m.length; i++) {
+      OutMethod om = new OutMethod();
+      list.add(om);
+      // int m1 = m[i].getModifiers();
+      om.modifiers = getModifiers(m[i].getModifiers());
+      om.type = getType(m[i].getReturnType());// m[i].getReturnType().getName();
+      om.name = m[i].getName();
+      om.typeParameters = this.getTypeParameters(m[i].getTypeParameters());
+      // System.out.print(m[i].getReturnType().getName());
+      // System.out.print(" " + m[i].getName());
+      // System.out.print("(");
+      // Class type[] = m[i].getParameterTypes();
+      // Parameter[] params = m[i].getParameters();
+
+      om.parameters = getParameters(m[i].getParameters());
+      // System.out.print(")");
+      // System.out.println(" ");
+    }
+    // } catch (Exception e) {
+    // }
+    return list;
+
+  }
+
+  public OutType getType(Class<?> c) {
+    OutType t = new OutType();
+    t.name = c.getName();
+    t.genericString = c.toGenericString();
+    t.typeParameters = this.getTypeParameters(c.getTypeParameters());
+    return t;
+  }
+  public List<String> getTypeParameters(TypeVariable[] t) {
+    List<String> list = new ArrayList<String>();
+    for (int i = 0; i < t.length; i++) {
+      // t[i].ty
+      list.add(t[i].toString());
+    }
+    return list;
   }
 
   Class<?> c;
@@ -173,21 +220,24 @@ public class JavapJsonOutput {
   public OutClass buildClass(String className) throws Exception {
     OutClass outClass = new OutClass();
     // OutClass c= printStart(className);
-    System.out.println("Compiled from  " + className);
+    // System.out.println("Compiled from " + className);
     // try {
     this.c = Class.forName(className);
     // int m = this.c.getModifiers();
     // List<String> mods = printModifier(m);
     outClass.modifiers = getModifiers(this.c.getModifiers());
     outClass.name = className;
-    System.out.print(" class " + className + "  ");
-
-    outClass.superClass = getSuperClass(className);
-    outClass.interfaces = getInterfaces(className);
-    outClass.fields = getFields(className);
-    outClass.constructors = getConstructors(className);
-    outClass.methods = getMethods(className);
-    System.out.print("} ");
+    outClass.typeParameters = this.getTypeParameters(this.c.getTypeParameters());
+    // System.out.print(" class " + className + " ");
+    outClass.isInterface = this.c.isInterface();
+    outClass.superClass = getSuperClass();
+    outClass.interfaces = getInterfaces();
+    outClass.fields = getFields();
+    outClass.constructors = getConstructors();
+    outClass.methods = getMethods();
+    outClass.genericString = this.c.toGenericString();
+    // this.c.doc
+    // System.out.print("} ");
     // } catch (Exception e) {
     // }
     return outClass;
@@ -225,33 +275,50 @@ public class JavapJsonOutput {
     }
   }
 
-  static class OutClass {
+  static class BaseNode {
     String name;
-    String type;
+    OutType type;
+  }
+
+  static class OutClass extends BaseNode {
+    boolean isInterface;
     List<OutMethod> methods;
     List<OutField> fields;
     List<OutMethod> constructors;
+    OutSuperClass superClass;
+    List<OutImplementedInterface> interfaces;
     List<String> modifiers;
-    String superClass;
-    List<String> interfaces;
+    List<String> typeParameters;
+    String genericString;
   }
 
-  static class OutField {
+  static class OutImplementedInterface extends BaseNode {
+    List<String> typeParameters;
+
+  } 
+  static class OutType {
     String name;
-    String type;
+    List<String> typeParameters;
+    String genericString;
+
+  }
+  static class OutSuperClass extends BaseNode {
+
+    List<String> typeParameters;
+  }
+  
+  static class OutField extends BaseNode {
     List<String> modifiers;
   }
 
-  static class OutMethod {
-    String name;
-    String type;
+  static class OutMethod extends BaseNode {
     List<String> modifiers;
     List<OutParam> parameters;
+    List<String> typeParameters;
   }
 
-  static class OutParam {
-    String name;
-    String type;
+  static class OutParam extends BaseNode {
+    List<String> modifiers;
   }
 
   // static class MyBuilder extends ReflectionToStringBuilder {
