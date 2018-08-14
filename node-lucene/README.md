a0db2a0f69f2bc9ca17145bddc5be82b500bf3f0
Sebastián Gurin <sebastigurin@gmail.com>
2018-08-14 18:56:36 -0300

    fs example in readme

--- node-lucene/README.md
+++ node-lucene/README.md
@@ -8,10 +8,12 @@ nodejs API to apache lucene Java implementation.
  * Requirements: Node.js and java
 
 # Usage
+
 ```sh
 npm install --save node-lucene
 ```
 
+## Example: indexing and searching in RAM 
 
 ```ts
 const lucene = require('node-lucene')
@@ -77,6 +79,27 @@ topDocs = searcher.search(parser.parse('cold'), 10) //TODO: lucene issue : why s
 const foundDoc = searcher.doc(topDocs.scoreDocs[0].doc)
 console.log(`Found "${foundDoc.get('title')}" authored by ${foundDoc.get('author')}`);
 
+```
+
+
+## Example: indexing in File System
+
+```typescript
+
+import * as lucene from 'node-lucene'
+import * as java from 'node-java-rt'
+
+const indexFolder = java.nio.file.Paths.getSync('.', 'tmp', 'index1')
+let directory = lucene.store.FSDirectory.open(indexFolder)
+let analyzer = new lucene.analysis.standard.StandardAnalyzer()
+let writerConfig = new lucene.index.IndexWriterConfig(analyzer)
+writerConfig.setOpenMode(lucene.index.IndexWriterConfig$OpenMode.CREATE_OR_APPEND)
+const writer = new lucene.index.IndexWriter(directory, writerConfig);
+const doc1 = new lucene.document.Document()
+const field1 = new lucene.document.TextField('content', 'hello world', lucene.document.FieldStore.YES)
+doc1.addSync(field1)
+writer.addDocumentSync(doc1)
+writer.closeSync()
 ```
 
 # Status 
--- node-lucene/spec/store/FSDirectorySpec.ts
+++ node-lucene/spec/store/FSDirectorySpec.ts
@@ -1,44 +1,35 @@
 import * as lucene from '../../src'
-import Directory from '../../src/store/Directory';
-import FSDirectory from '../../src/store/FSDirectory';
-import Paths from 'node-java-rt/dist/src/java/nio/file/Paths';
-import IndexWriterConfig$OpenMode from '../../src/index/IndexWriterConfig$OpenMode';
-import IndexWriter from '../../src/index/IndexWriter';
+import * as java from 'node-java-rt'
 import { rm, test } from 'shelljs';
 
 describe('FSDirectory', () => {
 
-
   it('should create folder in FS after adding docs and closing it', done => {
+    const indexFolder = java.nio.file.Paths.getSync('.', 'tmp', 'index1')
 
-    rm('-rf', Paths.getSync('.', 'tmp', 'index1').toString())
-    expect(test('-d', Paths.getSync('.', 'tmp', 'index1').toString())).toBeFalsy()
+    rm('-rf', indexFolder.toString())
+    expect(test('-d', indexFolder.toString())).toBeFalsy()
 
-    let directory: Directory = FSDirectory.open(Paths.getSync('.', 'tmp', 'index1'))
+    let directory = lucene.store.FSDirectory.open(indexFolder)
     let analyzer = new lucene.analysis.standard.StandardAnalyzer()
     let writerConfig = new lucene.index.IndexWriterConfig(analyzer)
-    writerConfig.setOpenMode(IndexWriterConfig$OpenMode.CREATE_OR_APPEND)
+    writerConfig.setOpenMode(lucene.index.IndexWriterConfig$OpenMode.CREATE_OR_APPEND)
 
-    const writer: IndexWriter = new IndexWriter(directory, writerConfig);
+    const writer = new lucene.index.IndexWriter(directory, writerConfig);
 
     const doc1 = new lucene.document.Document()
     const field1 = new lucene.document.TextField('content', 'hello world', lucene.document.FieldStore.YES)
 
-    const doc2 = new lucene.document.Document()
-    const field2 = new lucene.document.TextField('content', 'hi world', lucene.document.FieldStore.YES)
-
     doc1.addSync(field1)
     writer.addDocumentSync(doc1)
-    doc2.addSync(field2)
-    writer.addDocumentSync(doc2)
 
     writer.closeSync()
-    expect(test('-d', Paths.getSync('.', 'tmp', 'index1').toString())).toBeTruthy()
-    rm('-rf', Paths.getSync('.', 'tmp', 'index1').toString())
-    expect(test('-d', Paths.getSync('.', 'tmp', 'index1').toString())).toBeFalsy()
+
+    expect(test('-d', indexFolder.toString())).toBeTruthy()
+    rm('-rf', indexFolder.toString())
+    expect(test('-d', indexFolder.toString())).toBeFalsy()
 
     done()
   })
 
-  // })
 })
